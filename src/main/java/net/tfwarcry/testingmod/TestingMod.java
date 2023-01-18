b02cd615d5f4f4aa9a4dbdd40a72cbf8dea9795e
TFWarcry <codyemaslin@gmail.com>
2023-01-19 02:08:45 +0800

    Initial commit

---
+++ src/main/java/net/tfwarcry/testingmod/TestingMod.java
@@ -0,0 +1,17 @@
+package net.tfwarcry.testingmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestingMod implements ModInitializer {
+	public static final String MOD_ID = "testingmod";
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/tfwarcry/testingmod/TestingModClient.java
@@ -0,0 +1,11 @@
+package net.tfwarcry.testingmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestingModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
